00ef9b3e5e7626a3fa8aadbd0d2d34b0fe223b18
Anton Fresher <fresher@yandex-team.ru>
2018-11-15 23:01:07 +0500

    Скриптик для формирования выборки

---
+++ get_sample.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from nltk.stem.snowball import RussianStemmer
+
+
+def phrase_to_bag_of_words(phrase, stopwords=set(stopwords.words('russian'))):
+    vectorizer = CountVectorizer()
+    vectorizer.fit_transform([phrase])
+
+    features = vectorizer.get_feature_names()
+
+    return [word for word in features if not word in stopwords]
+    
+def jaccard_distance(a, b):
+    a_features = phrase_to_bag_of_words(a)
+    b_features = phrase_to_bag_of_words(b)
+
+    a_set = set(a_features)
+    b_set = set(b_features)
+
+    intersection = a_set.intersection(b_set)
+
+    return len(intersection) / (len(a_set) + len(b_set) - len(intersection))
+
+
+def gain_all_data():
+    return pd.read_csv('./data/all.csv')
+
+
+if __name__ == "__main__":
+    data = gain_all_data().sample(frac=1)
+
+    sample = [data.text[0]]
+
+    for phrase in data.text:
+        if len(sample) == 600:
+            break
+
+        distances = [jaccard_distance(phrase, text) < 0.6 for text in sample]
+
+        if all(distances):
+            sample.append(phrase)
+    
+    series = pd.Series(data=sample)
+    series.to_csv('./data/sample_600.csv')
