6f511c9f58148a2aa94bc3f3aeb58038282d7301
aryzhkin <aryzhkin@skbkontur.ru>
2019-01-02 01:45:14 +0500

    delete useless features and improve other

--- src/learn.py
+++ src/learn.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # coding=utf-8
-import math
 import pickle
 from functools import partial
 from itertools import combinations
@@ -9,11 +8,12 @@ from typing import Iterable, Sequence
 import numpy as np
 import pandas as pd
 import pymorphy2 as pm
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import accuracy_score
-
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.preprocessing import FunctionTransformer
@@ -22,10 +22,12 @@ from wiki_ru_wordnet import WikiWordnet
 from src.maru.grammeme import Gender, Animacy
 from src.maru.grammeme.pos import PartOfSpeech
 from src.maru.morph import Morph
+import gensim.downloader as api
 
 Morphs = Sequence[Sequence[Morph]]
 wikiwordnet = WikiWordnet()
 morph = pm.MorphAnalyzer()
+word_vectors = api.load("word2vec-ruscorpora-300")
 
 FILTERED_POS = {
     PartOfSpeech.UNKNOWN,
@@ -47,15 +49,6 @@ MEANINGFUL_POS = {
 }
 
 
-def get_normalized_words(text):
-    words = []
-    for word in text.split():
-        res = morph.parse(word)
-        if res[0].tag.POS in MEANINGFUL_POS:
-            words.append(res[0].normal_form)
-    return words
-
-
 def get_words_synset(word):
     wordsSet = set()
     wikiwordnet.get_synsets(word)
@@ -67,23 +60,14 @@ def get_words_synset(word):
     return wordsSet
 
 
-def get_syn_count(joke):
-    words = set(get_normalized_words(joke))
+def get_syn_count(x: Sequence[Morph]):
+    words = set([m.lemma for m in x if m.tag.pos in MEANINGFUL_POS])
     synsets = [(x, get_words_synset(x)) for x in words]
     words_with_syn = [w for w in synsets if len(w[1]) > 0]
     if len(words_with_syn) <= 1:
         return 0
     kek = [a for a in combinations(words_with_syn, 2) if a[0][0] != a[1][0] and len(a[0][1].intersection(a[1][1])) > 0]
     return len(kek)
-
-
-def get_unknown_words(text):
-    words = []
-    for word in text.split():
-        res = morph.parse(morph.normal_forms(word)[0])
-        if res[0].tag.POS in MEANINGFUL_POS and len(wikiwordnet.get_synsets(morph.normal_forms(word)[0])) == 0:
-            words.append(word)
-    return len(words)
 
 
 def read_dump(file_path: str) -> Iterable[Sequence['Morph']]:
@@ -106,7 +90,7 @@ def select_text(x: pd.Series):
 def gain_train_data(source_path):
     df = pd.read_csv(source_path, delimiter=';').dropna()
     text_index = select_text_index(df)
-    return train_test_split(text_index, df.category, test_size=0.3)
+    return train_test_split(text_index, df.category, test_size=0.3, shuffle=True, random_state=16)
 
 
 def get_text_length(x: pd.Series):
@@ -122,11 +106,13 @@ def contains_question1(x: pd.Series):
 
 
 def syn_count(x: pd.Series):
-    return np.array([get_syn_count(t) for t in x]).reshape(-1, 1)
+    return np.array([
+        get_unknown_words_count(m) for line in x for m in line
+    ]).reshape(-1, 1)
 
 
-def get_sex_keywords_count(joke):
-    words = get_normalized_words(joke)
+def get_sex_keywords_count(x: Sequence[Morph]):
+    words = [m.lemma for m in x if m.tag.pos in MEANINGFUL_POS]
     sex_keywords = ["секс", "член", "трах", "жена", "муж", "парень", "любовь", "оргазм",
                     "девушка", "трах", "постель", "измена", "женщина", "мужчина", "порно", "минет", "анал", "дроч"]
     res = len([word for word in words if word in sex_keywords or any([sex_word in word for sex_word in sex_keywords])])
@@ -135,11 +121,20 @@ def get_sex_keywords_count(joke):
 
 
 def sex_topic_words_count(x: pd.Series):
-    np.array([get_sex_keywords_count(t) for t in x]).reshape(-1, 1)
+    return np.array([
+        get_sex_keywords_count(m) for line in x for m in line
+    ]).reshape(-1, 1)
 
 
-def unknown_words_count(x: pd.Series):
-    return np.array([get_unknown_words(t) for t in x]).reshape(-1, 1)
+def get_mean_w2v(x: Sequence[Morph]):
+    words = [f"{m.lemma}_{m.tag.pos}" for m in x if f"{m.lemma}_{m.tag.pos}" in word_vectors]
+    return np.mean([word_vectors[word] for word in words], axis=0)
+
+
+def mean_w2v(x: pd.Series):
+    return np.array([
+        get_mean_w2v(m) for line in x for m in line
+    ]).reshape(-1, 1)
 
 
 def get_morphs(morphs: Morphs, x: pd.Series):
@@ -163,6 +158,16 @@ def get_unique_count(x: Sequence[Morph]):
 def unique_words(x: pd.Series):
     return np.array([
         get_unique_count(m) for line in x for m in line
+    ]).reshape(-1, 1)
+
+
+def get_unknown_words_count(x: Sequence[Morph]):
+    return len([m.lemma for m in x if m.tag.pos in MEANINGFUL_POS and f"{m.lemma}_{m.tag.pos}" not in word_vectors])
+
+
+def unknown_words_count(x: pd.Series):
+    return np.array([
+        get_unknown_words_count(m) for line in x for m in line
     ]).reshape(-1, 1)
 
 
@@ -199,33 +204,23 @@ def get_meaningful_pos(x: pd.Series):
 
 
 def create_model(morphs: Morphs):
-    tfidf_pipeline = Pipeline([
-        ('column', FunctionTransformer(select_text, validate=False)),
-        ('vect', CountVectorizer(ngram_range=(1, 2))),
-        ('tfidf', TfidfTransformer())
-    ])
-
+    russian_stopwords = stopwords.words("russian")
     lemma_tfidf_pipeline = Pipeline([
         ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
         ('lemmas', FunctionTransformer(get_lemmas, validate=False)),
-        ('vect', CountVectorizer(ngram_range=(1, 2))),
+        ('vect', TfidfVectorizer(stop_words=russian_stopwords, ngram_range=(1, 2))),
         ('tfidf', TfidfTransformer())
-    ])
-
-    length_pipeline = Pipeline([
-        ('column', FunctionTransformer(select_text, validate=False)),
-        ('length', FunctionTransformer(get_text_length, validate=False))
     ])
 
     count_vec_lemma_pipeline = Pipeline([
         ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
         ('lemmas', FunctionTransformer(get_lemmas, validate=False)),
-        ('vectorizer', CountVectorizer(ngram_range=(1, 2)))
+        ('vectorizer', TfidfVectorizer(stop_words=russian_stopwords, ngram_range=(1, 2)))
     ])
 
     count_vec_pipeline = Pipeline([
         ('column', FunctionTransformer(select_text, validate=False)),
-        ('vectorizer', CountVectorizer(ngram_range=(1, 2)))
+        ('vectorizer', TfidfVectorizer(stop_words=russian_stopwords, ngram_range=(1, 2)))
     ])
 
     unique_words_pipeline = Pipeline([
@@ -233,24 +228,14 @@ def create_model(morphs: Morphs):
         ('counts', FunctionTransformer(unique_words, validate=False))
     ])
 
+    unknown_words_pipeline = Pipeline([
+        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
+        ('counts', FunctionTransformer(unknown_words_count, validate=False))
+    ])
+
     gender_pipeline = Pipeline([
         ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
         ('lol_counts', FunctionTransformer(gender_words_count, validate=False))
-    ])
-
-    verb_pipeline = Pipeline([
-        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
-        ('pos_ratio', FunctionTransformer(partial(get_part_of_speech_ratio, PartOfSpeech.VERB), validate=False))
-    ])
-
-    noun_pipeline = Pipeline([
-        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
-        ('pos_ratio', FunctionTransformer(partial(get_part_of_speech_ratio, PartOfSpeech.NOUN), validate=False))
-    ])
-
-    adj_pipeline = Pipeline([
-        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
-        ('pos_ratio', FunctionTransformer(partial(get_part_of_speech_ratio, PartOfSpeech.ADJECTIVE), validate=False))
     ])
 
     pos_pipeline = Pipeline([
@@ -267,38 +252,34 @@ def create_model(morphs: Morphs):
         ('quest1', FunctionTransformer(contains_question1, validate=False))
     ])
     syn_pipeline = Pipeline([
-        ('column', FunctionTransformer(select_text, validate=False)),
-        ('quest', FunctionTransformer(syn_count, validate=False))
+        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
+        ('syn', FunctionTransformer(syn_count, validate=False))
     ])
 
     sex_topic_words_count_pipeline = Pipeline([
-        ('column', FunctionTransformer(select_text, validate=False)),
+        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
         ('quest', FunctionTransformer(sex_topic_words_count, validate=False))
     ])
 
-    unknow_words_pipeline = Pipeline([
-        ('column', FunctionTransformer(select_text, validate=False)),
-        ('unkn', FunctionTransformer(unknown_words_count, validate=False))
+    mean_w2v_pipeline = Pipeline([
+        ('morphs', FunctionTransformer(partial(get_morphs, morphs), validate=False)),
+        ('counts', FunctionTransformer(mean_w2v, validate=False))
     ])
 
     return Pipeline([
         ('features', FeatureUnion([
-            ('tfidf', tfidf_pipeline),
             ('lemma_tfidf', lemma_tfidf_pipeline),
             ('count_vec_lemma', count_vec_lemma_pipeline),
             ('count_vec', count_vec_pipeline),
-            ('length', length_pipeline),
             ('unique_words', unique_words_pipeline),
-            ('adj', adj_pipeline),
-            ('noun', noun_pipeline),
-            ('verb', verb_pipeline),
             ('meaningful_pos', pos_pipeline),
             ('quest', quest_pipeline),
             ("quest1", quest_pipeline1),
             ('syn', syn_pipeline),
-            ('unkn', unknow_words_pipeline),
+            ('unkn', unknown_words_pipeline),
             ('gen', gender_pipeline),
-            ('sex_keywords', sex_topic_words_count_pipeline)
+            ('sex_keywords', sex_topic_words_count_pipeline),
+            #('mean_w2v', mean_w2v_pipeline),
         ])),
         ('lr', LogisticRegression(solver="lbfgs", multi_class="auto", max_iter=400))
     ])
